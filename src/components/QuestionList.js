9fa948f464c6d74bf8e03c208f1d95fb115c6e24
Pete Fowler <fowler.pete@gmail.com>
2022-09-02 17:26:51 -0600

    Get and display questions

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-function QuestionList() {
+function QuestionList({ questions }) {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      {questions.map(question => 
+        <ul>
+          <li>
+            <p>Prompt {question.prompt}</p>
+            <p>Answers: {question.answers.map(ans => `${ans}  `)}</p>
+            <p>Correct: {question.answers[question.correctIndex]}</p>
+          </li>
+        </ul>
+      )}
     </section>
   );
 }
